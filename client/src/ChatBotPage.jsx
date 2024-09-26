840e250cae4aff670204d944ba5c6dc2207ca80f
Guilherme Buys <guibuys55@gmail.com>
2024-09-26 10:23:40 -0300

    começando a lógica do chatbot

---
+++ client/src/ChatBotPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ChatBot() {
+    const [messages, setMessages] = React.useState('');
+    const [responses, setResponses] = React.useState([]);
+
+    return (
+        <h1>Hello World</h1>
+    )
+}
