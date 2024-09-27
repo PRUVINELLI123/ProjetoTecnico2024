9875681516eadf7854fb2c9df37b2f9c0aca5db8
Guilherme Buys <guibuys55@gmail.com>
2024-09-27 11:11:28 -0300

    logica de submit

--- client/src/ChatBotPage.jsx
+++ client/src/ChatBotPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { getResponse } from '../../server/src/index'
 
 export default function ChatBot() {
     const [messages, setMessages] = React.useState('');
     const [responses, setResponses] = React.useState([]);
+
+    function handleSubmit(event, message) {
+        event.preventDefault()
+    
+        let userMessage = message
+        var prompt = userMessage.ariaValueMax.trim()
+    
+        if (prompt === "") {
+            return;
+        }
+    }
 
     return (
         <h1>Hello World</h1>
--- server/src/index.js
+++ server/src/index.js
@@ -3,25 +3,18 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(`${import.meta.env.GEMINI_API_KEY}`)
 const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
 
-async function mainChat() {
-    const message = await model.generateContent('hi')
+async function mainChat(region) {
+    const message = await model.generateContent(
+        `Você está restrito a falar somente em italiano sobre a cultura da região ${region}`
+    )
     const response = await message.response
     const text = response.text()
 
     console.log(text)
 }
 
-mainChat()
-
-function getResponse(prompt) {}
-
-function handleSubmit(event, message) {
-    event.preventDefault()
-
-    let userMessage = message
-    var prompt = userMessage.ariaValueMax.trim()
-
-    if (prompt === "") {
-        return;
-    }
+export async function getResponse(prompt) {
+    const message = await model.generateContent(prompt)
+    const response = await message.response
+    const text = response.text()
 }
