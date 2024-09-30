d6694ff4f887f112281bd57dc2a614781414c525
Guilherme Buys <guibuys55@gmail.com>
2024-09-30 09:52:53 -0300

    removendo provider de tema (nao vamos usar) e adição de novos sistemas no chatbot

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,34 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(`${import.meta.env.GEMINI_API_KEY}`)
-const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
+const genAI = new GoogleGenerativeAI(`${import.meta.env.GEMINI_API_KEY}`);
+const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
 async function mainChat(region) {
     const message = await model.generateContent(
         `Você está restrito a falar somente em italiano e só sobre a cultura e a região de ${region}`
-    )
-    const response = await message.response
-    const text = response.text()
+    );
 
-    console.log(text)
+    const response = await message.response;
+    const text = response.text();
+
+    console.log(text);
+};
+
+const AIResponseDiv = (ai_response) => {
+    return (
+        <div>
+            <p>{ai_response}</p>
+        </div>
+    )
 }
 
+mainChat();
+
 export async function getResponse(prompt) {
-    const message = await model.generateContent(prompt)
-    const response = await message.response
-    const text = response.text()
+    const message = await model.generateContent(prompt);
+
+    const response = await message.response;
+    const text = response.text();
+
+    AIResponseDiv(text);
 }
