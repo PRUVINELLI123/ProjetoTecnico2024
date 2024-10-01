8c3765dea53426488981489723d96b482950100d
Guilherme Buys <guibuys55@gmail.com>
2024-10-01 08:39:41 -0300

    atualização api chatbot

--- client/src/ChatBotPage.jsx
+++ client/src/ChatBotPage.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import { getResponse } from '../../server/src/index'
 
 export default function ChatBot() {
+    const api_key = "AIzaSyBidIQR-AhzrtAf_CRZHhJ6XvpLVyo2cg0"
+    const api_url = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${api_key}`
+
     const [message, setMessage] = React.useState('');
+
+    let userFinalMessage = null
+
+    async function generateResponse () {
+        try {
+            const response = await fetch(api_url, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    contents: [{
+                        role: "user",
+                        parts: [{ text: userFinalMessage }]
+                    }]
+                })
+            })
+
+            const data = await response.json()
+            const apiResponse = data?.candidates[0].content.parts[0].text
+
+            console.log(apiResponse)
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        getResponse(message);
+        generateResponse(message);
     }
 
     return (
